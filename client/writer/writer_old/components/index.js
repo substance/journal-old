681dba23359400fa5acbabdf9147140884ad99e5
Michael Aufreiter <michael@substance.io>
2015-06-03 18:59:04 +0200

    Delete legacy stuff.

--- client/writer/writer_old/components/index.js
+++
@@ -1,7 +0,0 @@
-var ParagraphComponent = require("./paragraph_component");
-var HeadingComponent = require("./heading_component");
-
-module.exports = {
-  "paragraph": ParagraphComponent,
-  "heading": HeadingComponent
-};
