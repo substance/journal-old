85b56ea7c0a67fb02bcfdd020aefea12d4d2d3a8
Michael Aufreiter <michael@substance.io>
2015-06-08 17:28:31 +0200

    Use hashed password for default user.

--- server/db/seeds/01_default_user.js
+++ server/db/seeds/01_default_user.js
@@ -1,8 +1,16 @@
+var bcrypt = require('bcrypt');
+
+var INSECURE_DEMO_PASSWORD = "123456";
+
+
+var salt = bcrypt.genSaltSync(10);
+var hashedPassword = bcrypt.hashSync(INSECURE_DEMO_PASSWORD, salt);
+
 exports.seed = function(knex, Promise) {
   return Promise.join(
     knex('users').insert({
       username: "admin",
-      password: "123456",
+      password: hashedPassword,
       name: "Administrator",
       bio: "I'm the Substance Journal System administrator.",
       location: "Linz",
